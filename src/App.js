eb60a706c7c5f3eaaefbc6ce7e9f17e886c60e23
moh1idris <113855948+moh1idris@users.noreply.github.com>
2022-10-15 12:34:07 +0530

    buttons's been  activated

--- src/App.js
+++ src/App.js
@@ -36,6 +36,14 @@ const {company, dates, duties, title} = jobs[value];
     </div>
     <div className="jobs-center">
       {/* btn container */}
+      <div className="btn-container">
+
+      {jobs.map((item,index)=>{
+        return( 
+          <button key={item.id} onClick={()=>setValue(index)} className={`job-btn ${index === value && 'active-btn'}`}>{item.company}</button>
+          );
+        })}
+        </div>
       {/* job info */}
       <article className='job-info'>
         <h3>{title}</h3>
